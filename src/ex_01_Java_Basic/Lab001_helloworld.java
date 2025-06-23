4ddfd83c4afe7b56dd09fe0dbf4b8b507dcf1705
preetamrevankar <preetam.revankar@juspay.in>
2025-06-23 12:09:19 +0530

    shortcut

--- src/ex_01_Java_Basic/Lab001_helloworld.java
+++ src/ex_01_Java_Basic/Lab001_helloworld.java
@@ -1,4 +1,7 @@
 package ex_01_Java_Basic;
 
 public class Lab001_helloworld {
+    public static void main(String[] args) {
+        System.out.printf("helloworld");
+    }
 }
---
+++ src/ex_01_Java_Basic/Lab002_shortcut.java
@@ -0,0 +1,10 @@
+ package ex_01_Java_Basic;
+
+ public class Lab002_shortcut {
+    public static void main (String[] args ){
+        System.out.println("Hello test");
+    }
+    public static void main (int args){
+
+    }
+}
