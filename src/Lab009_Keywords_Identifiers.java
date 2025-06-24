e3255b52099b79d63e0d4a1992f308c99d9083f3
preetamrevankar <preetam.revankar@juspay.in>
2025-06-24 17:44:31 +0530

    added new program

---
+++ src/Lab009_Keywords_Identifiers.java
@@ -0,0 +1,5 @@
+public class Lab009_Keywords_Identifiers {
+    public static void main(String[] args) {
+        System.out.println("Hi,preetam");
+    }
+}
---
+++ src/ex_01_Java_Basic/Lab010_Keywords_Identifiers2.java
@@ -0,0 +1,7 @@
+package ex_01_Java_Basic;
+
+public class Lab010_Keywords_Identifiers2 {
+    public static void main(String[] args) {
+        System.out.println("how many keywords do you see ?,identifier");
+    }
+}
