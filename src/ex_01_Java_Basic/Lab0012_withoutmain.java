636b31c8afdebd5f17d387ccf722ce2463fdccab
preetamrevankar <preetam.revankar@juspay.in>
2025-07-21 09:59:49 +0530

    added new program today jul 21

---
+++ src/ex_01_Java_Basic/Lab0012_withoutmain.java
@@ -0,0 +1,4 @@
+package ex_01_Java_Basic;
+
+public class Lab0012_withoutmain {
+}
---
+++ src/ex_01_Java_Basic/Lab013_this_is_very_Long_name.java
@@ -0,0 +1,7 @@
+package ex_01_Java_Basic;
+
+public class Lab013_this_is_very_Long_name {
+    public static void main(String[] args) {
+        System.out.println("this_is_very_long_name");
+    }
+}
