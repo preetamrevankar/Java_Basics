9ec8801e9ca03549b49cd972a8d1e1c2556bb283
preetamrevankar <preetam.revankar@juspay.in>
2025-07-23 11:12:04 +0530

    added new program today jul 23 part 1

---
+++ src/ex_020_polymorphism/Lab188_REAL_MOverloading.java
@@ -0,0 +1,23 @@
+package ex_020_polymorphism;
+
+public class Lab188_REAL_MOverloading {
+    public static void main(String[] args) {
+        home h1 = new home();
+        h1.task(3);
+        h1.task(true);
+
+    }
+}
+
+class home {
+    void task (){
+        System.out.println("task1");
+    }
+    int task(int a){
+        System.out.println("task1");
+        return a;
+    }
+    boolean task(boolean a){
+       return false;
+    }
+}
---
+++ src/ex_020_polymorphism/WebAutomation.java
@@ -0,0 +1,18 @@
+package ex_020_polymorphism;
+
+public class WebAutomation {
+    public static void main(String[] args) {
+        browser p1 = new browser();
+       p1.StartBrowser("chrome");
+    }
+}
+
+class browser{
+    void StartBrowser(){
+        System.out.println("staring the default browser");
+    }
+   String StartBrowser (String browser){
+       System.out.println("starting  browser"+browser);
+       return browser;
+   }
+        }
