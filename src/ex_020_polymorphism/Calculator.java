86a92cc3c07896b68385fcd880fdcc290f2121ad
preetamrevankar <preetam.revankar@juspay.in>
2025-07-23 09:55:14 +0530

    added new program today jul 23

---
+++ src/ex_020_polymorphism/Calculator.java
@@ -0,0 +1,23 @@
+package ex_020_polymorphism;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Calc C1 = new Calc();
+        C1.add(3, 4);
+        C1.add(3.14, 5.84);
+        System.out.println(C1.add(3,4));
+        System.out.println(C1.add(3.156,6883.4));
+    }
+}
+
+    class Calc {
+       public int add (int a, int b){
+           return a+b;
+       }
+
+       public double add (double a, double b ){
+           return a+b;
+       }
+    }
+
+
