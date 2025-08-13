a2481a585f8dbcf2069f6a8b80788d2477ddf7ff
preetamrevankar <preetam.revankar@juspay.in>
2025-08-13 22:27:22 +0530

    abstructor

---
+++ src/ex_30_exception/Lab220_Handle_exception.java
@@ -0,0 +1,17 @@
+package ex_30_exception;
+
+import java.util.Scanner;
+
+public class Lab220_Handle_exception {
+    public static void main(String[] args) {
+        Scanner scanner = new scanner() ;
+        int a=0 ;
+        try {
+            a=10/0 ;
+        } catch (Exception e) {
+            System.out.println("Div by zero is not allowed");
+        }
+
+        System.out.println(a);
+    }
+}
